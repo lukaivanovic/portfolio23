ee8c28d9648bec8842844aeddb545e3a5b0b2ed7
lukaivanovic <lluka.ivanovic@gmail.com>
2023-03-17 15:39:50 +0100

    all

--- pages/index.js
+++ pages/index.js
@@ -39,12 +39,12 @@ export default function Home() {
           <p>Luka Ivanović</p>
           <h1>Product design for startups and early-stage companies</h1>
           <p>Independent functionalist passionate about building stuff. Designing for four years. Trying to code here and there. Currently based in the EU.</p>
-          <p>Notable previous work includes an m-banking app for a big European bank, all digital products for a Nigerian fintech that was acquired, and a mobile app that blocks spam calls in the US.</p>
+          <p>Previous work includes an m-banking app for a big European bank, acquired loan fintech and a US spam call blocking mobile app.</p>
         </div>
 
         <div className={styles.projects}>
-          <Project title= "WeWeb"  description= "No-code supercharged app builder" url = "weweb" image = '/weweb2.png' link="https://www.weweb.io" linkName="weweb.io"/>
-          <Project title= "Digital Assets" description= "Buy, sell and store cryptocurrencies" url = "bitcoin-store" image = '/digital-assets.png' link="https://www.bitcoin-store.net" linkName="bitcoin-store.net"/>
+          <Project title= "WeWeb"  description= "No-code supercharged app builder" url = "weweb" image = '/weweb2.png' previewImage = '/weweb-preview.png' link="https://www.weweb.io" linkName="weweb.io"/>
+          <Project title= "Digital Assets" description= "Buy, sell and store cryptocurrencies" url = "bitcoin-store" image = '/digital-assets.png' previewImage = '/bitcoin-preview.png' link="https://www.bitcoin-store.net" linkName="bitcoin-store.net"/>
         </div>
       
         <div className='divider'></div>

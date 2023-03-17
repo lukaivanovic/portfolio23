30263743a0eaa32259877060c15484d9bee31d75
lukaivanovic <lluka.ivanovic@gmail.com>
2023-03-17 15:42:00 +0100

    webp

--- pages/index.js
+++ pages/index.js
@@ -43,8 +43,8 @@ export default function Home() {
         </div>
 
         <div className={styles.projects}>
-          <Project title= "WeWeb"  description= "No-code supercharged app builder" url = "weweb" image = '/weweb2.png' previewImage = '/weweb-preview.png' link="https://www.weweb.io" linkName="weweb.io"/>
-          <Project title= "Digital Assets" description= "Buy, sell and store cryptocurrencies" url = "bitcoin-store" image = '/digital-assets.png' previewImage = '/bitcoin-preview.png' link="https://www.bitcoin-store.net" linkName="bitcoin-store.net"/>
+          <Project title= "WeWeb"  description= "No-code supercharged app builder" url = "weweb" image = '/weweb2.png' previewImage = '/weweb-preview.webp' link="https://www.weweb.io" linkName="weweb.io"/>
+          <Project title= "Digital Assets" description= "Buy, sell and store cryptocurrencies" url = "bitcoin-store" image = '/digital-assets.png' previewImage = '/bitcoin-preview.webp' link="https://www.bitcoin-store.net" linkName="bitcoin-store.net"/>
         </div>
       
         <div className='divider'></div>
